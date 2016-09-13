70b3eafe9e06a2040792fc44b86acc03a8fab046
Omid <omid.alemi@gmail.com>
2016-09-12 18:30:27 -0700

    rewrote the triSelect draw/click/drag

--- js/triSelect.js
+++ js/triSelect.js
@@ -7,34 +7,54 @@ Created: September 2, 2016
 */
 
 (function ( $ ) {
-    var controlWidth = 80;
+    var controlWidth = 100;
     var thumSize = 12;
     var container;
-    var h = controlWidth * (Math.sqrt(3)/2);
+    // var h = controlWidth * (Math.sqrt(3)/2);
+    var sideWidth = controlWidth - thumSize;
+    var triHeight = sideWidth*(Math.sqrt(3)/2);
     var contInput = true; 
+    var sqrt3 = Math.sqrt(3);
 
     var defaults = {
         input : function() {},        
     };
 
-    var methods = {        
-        value : function() {              
-            t = this.children(".thumb");
+    var methods = {
+        value : function() {
+            // it has to be the distance from the thumb to each of the corners            
+            t = this.children(".thumb");        
+
             posX = parseInt(t.css('left').substr(0,t.css('left').length-2));
             posX = Math.max(0,-10 + posX);
-            posX = Math.min(controlWidth-thumSize/2, posX);
-            posY = Math.max(0,-10 + t.position().top - this.position().top);
-            console.log("h:"+h);
-            _top = (h - posY)/h;
-            _left = ((controlWidth-thumSize/2) - posX)/(controlWidth-thumSize/2);
-            _right = 1 - _left;  
-            console.log(posX + " , " + posY);
+            posX = Math.min(sideWidth, posX);
+
+            posY = Math.abs(controlWidth + parseInt(t.css('top').substr(0,t.css('top').length-2)));
+            posY = Math.max(0,posY);        
+            posY = Math.min(triHeight,posY);
+  
+            // console.log('>>x '+ posX);
+            // console.log('>>y '+ posY);
+
+            topDist = Math.round(triHeight) - posY;
+            rightDist = Math.sqrt(Math.max(0,Math.pow(posX,2) - Math.pow(topDist,2)));
+            leftDist = Math.sqrt(Math.max(0,Math.pow(sideWidth-posX,2) - Math.pow(topDist,2)));
+
+            // console.log('triHeight: '+triHeight);
+            // console.log('topDist: '+topDist);
+            // console.log('leftDist: '+leftDist);
+            // console.log('rightDist: '+rightDist);
+
+            _top = topDist/Math.round(triHeight);            
+            _left = leftDist/Math.round(triHeight);
+            _right = rightDist/Math.round(triHeight);
+                    
             return {
-                top: _top,
-                left: _left,
-                right: _right
+                top: Math.min(Math.max(_top,0),1),
+                left: Math.min(Math.max(_left,0),1),
+                right: Math.min(Math.max(_right,0),1)
             };
-          }        
+          }       
     };
 
     $.fn.triSelect = function( methodOrOptions ) {         
@@ -49,34 +69,39 @@ Created: September 2, 2016
         var settings = $.extend( {}, defaults, methodOrOptions );
         var container = this;
 
-        this.addClass("triP");
+        container.addClass("triP");
+        container.width(sideWidth+thumSize/2+2);
+        container.height(triHeight+thumSize/2+1);
 
         // Drawings
         var svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
-        this.append(svg);
+        svg.style.width = "100%";
+        svg.style.height = "100%";
+        svg.style.margin = "0";
+        container.append(svg);
        
         var triangle = document.createElementNS("http://www.w3.org/2000/svg","polyline");
-        triangle.setAttributeNS(null,"points",(thumSize/2+2)+","+h+" "+(controlWidth/2+2)+","+(thumSize/2+2)+" "+(controlWidth+2)+","+h+" "+(thumSize/2+2)+","+h);
+        triangle.setAttributeNS(null,"points",(thumSize/2+1)+","+triHeight+" "+(sideWidth/2+1)+","+(thumSize/2+1)+" "+(sideWidth+1)+","+triHeight+" "+(thumSize/2+1)+","+triHeight);
         triangle.setAttributeNS(null,"style","fill:#fafafa;stroke:#ababab;stroke-width:1");
         this.children("svg").append(triangle);
         
         var c1 = document.createElementNS("http://www.w3.org/2000/svg","circle");
-        c1.setAttributeNS(null,"cx",thumSize/2+2);
-        c1.setAttributeNS(null,"cy",h);
+        c1.setAttributeNS(null,"cx",thumSize/2+1);
+        c1.setAttributeNS(null,"cy",triHeight);
         c1.setAttributeNS(null,"r",thumSize/2);
         c1.setAttributeNS(null,"style","stroke:#ababab;stroke-width:1;fill:white");
         this.children("svg").append(c1);
 
         var c2 = document.createElementNS("http://www.w3.org/2000/svg","circle");
-        c2.setAttributeNS(null,"cx",controlWidth/2+2);
+        c2.setAttributeNS(null,"cx",sideWidth/2+1);
         c2.setAttributeNS(null,"cy",thumSize/2+1);
         c2.setAttributeNS(null,"r",thumSize/2);
         c2.setAttributeNS(null,"style","stroke:#ababab;stroke-width:1;fill:white");
         this.children("svg").append(c2);
 
         var c3 = document.createElementNS("http://www.w3.org/2000/svg","circle");
-        c3.setAttributeNS(null,"cx",controlWidth+2);
-        c3.setAttributeNS(null,"cy",h);
+        c3.setAttributeNS(null,"cx",sideWidth+1);
+        c3.setAttributeNS(null,"cy",triHeight);
         c3.setAttributeNS(null,"r",thumSize/2);
         c3.setAttributeNS(null,"style","stroke:#ababab;stroke-width:1;fill:white");
         this.children("svg").append(c3);
@@ -87,65 +112,53 @@ Created: September 2, 2016
         this.append(thumb);            
 
         // Dragging 
-        ww = thumb.offsetWidth/2;
-        hh = thumb.offsetHeight/2;
-        center = this.width()/2 - thumb.clientWidth/2 + 2;
-        console.log(this.width());
+        center = sideWidth/2 ;        
     
-        thumb.style.left = (center)+"px";
-        thumb.style.top = (-controlWidth+1)+ "px";
+        thumb.style.left = (center-thumSize/2+1)+"px";
+        thumb.style.top = (-triHeight-thumSize/2)+ "px";
         
         this.children(".thumb" ).draggable({
             containment: "parent",
-            drag: function(event, ui){                               
-                console.log(event);
-                console.log("center: "+center);
-                relY = event.clientY - container.position().top;
-                relY = Math.min(Math.max(10 + thumSize/2,relY),h+10 + thumSize/2 - ww);
-
-                triWidth = relY/(Math.sqrt(3)/2);
-
-                relX = (event.clientX - container.position().left)/2 ;
-                
-                a = Math.max((center - triWidth/2 + thumSize/2 +4 ),relX);                
-                relX = Math.min(a,(center + triWidth/2 - thumSize/2));
-
-                console.log("eventX diff: " + (event.clientX - container.position().left));
-                console.log("relX: "+ relX);                                                    
-                console.log("triWidth: "+triWidth);
-                console.log("a :" + ((controlWidth/2 - triWidth/2)));
-                console.log("b: "+ ((controlWidth/2 + triWidth/2)));                
-                
-                ui.position.left = relX;//Math.min(Math.max(center-triWidth/2,relX),center+triWidth/2) + 2;
-                ui.position.top = (-controlWidth+1) + relY - 10 - thumSize/2;//Math.min(Math.max(ww,relY),h+ww)-ww;
-                
-                if (contInput)
-                   container.trigger("input");                    
+            drag: function(event, ui){            
+                // nothing here anymore            
             },
             stop: function(event,ui) {
+                var $this = $(this);
+                
+                posX = parseInt($this.css('left').substr(0,$this.css('left').length-2));
+                relY = $this.position().top - container.position().top;                
+                triWidth = relY/(Math.sqrt(3)/2); 
+                if (posX < (center - triWidth/2 - thumSize/2  +1 )) { 
+                        $this.css('left', center - triWidth/2 - thumSize/2+2);                                           
+                } else if (posX > (center + triWidth/2 + 4 ) ) { 
+                        $this.css('left', center + triWidth/2 - thumSize/2+1);                      
+                }
+
+                posY = -parseInt($this.css('top').substr(0,$this.css('top').length-2));
+                if (posY > sideWidth-thumSize/2) 
+                    $this.css('top',-sideWidth+thumSize/2);
+                else if (posY <= thumSize)
+                     $this.css('top',-thumSize-1);
+
                 if (!contInput)
                    container.trigger("input"); 
             }
         }); 
 
-
         // Click to select
         this.children("svg").on("click", function(event){
             y = event.offsetY;
             triWidth = y/(Math.sqrt(3)/2);            
-            if (y < h && event.offsetX > (center - triWidth/2 + 2 - ww) &&
-                event.offsetX < (center + triWidth/2 + 2 + ww) ) {                    
-                    xx = event.offsetX+1;
-                    xx = Math.max(center - triWidth/2 + 2, xx);
-                    yy = (-controlWidth+1)+ event.offsetY-ww;
+            if (y < triHeight && event.offsetX > (center - triWidth/2 + 4 ) &&
+                event.offsetX < (center + triWidth/2 + 4 ) ) {                    
+                    xx = event.offsetX-thumSize/2+1;                    
+                    yy = (-sideWidth+1)+ event.offsetY;
                     container.children(".thumb").css('left',xx);
                     container.children(".thumb").css('top',yy);
-
-                    // settings.click.call("hahaha");
+                    
                     container.trigger("input");                    
                 }                
         });
-
         return this;
     };   
 }( jQuery ));
